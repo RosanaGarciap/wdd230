516fa4dad577fe682f144afef8cdbf89e20e13e6
Rosana Garcia <rdlagp@byui.edu>
2023-01-20 20:55:33 -0400

    Week 02: adding Dark mode, hamburger menu, and contrast colors fixed

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,42 @@ function last_modified_date() {
     document.getElementById("year").innerHTML =new Date().getFullYear()
 }
 last_modified_date();
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("body");
+const h2 = document.querySelector("h2");
+const h1 = document.querySelector("h1");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.checked) {
+		main.style.background = "#282A3A";
+        h2.style.color = "#D8bcab";
+        h1.style.color = "#272932";
+        header.style.color = "#272932";
+        header.style.background = "#D8bcab";
+        footer.style.color = "#272932";
+        footer.style.background = "#D8bcab";
+	} else {
+		main.style.background = "#fff";
+		main.style.color = "#272932";
+        h2.style.color = "#272932";
+        footer.style.color = "#735F32";
+        footer.style.background = "#272932";
+        header.style.background = "#272932";
+	}
+});
+
+/**
+ * Hamburger Menu
+ */
+
+const menubutton = document.querySelector(".menu-button");
+const menuitems = document.querySelectorAll(".menu-item");
+// menuitems will be a Node List of the list items
+
+menubutton.addEventListener("click", () => {
+	menuitems.forEach((item) => item.classList.toggle("menu-item"));
+	menubutton.classList.toggle("close");
+});
