7d91803adb5a77c6cbcc22daa03808e682e2cb84
RosanaGarciap <rdlagp@byui.edu>
2023-01-30 20:32:08 -0400

    Week 03 home page updating

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -43,3 +43,22 @@ menubutton.addEventListener("click", () => {
 	menuitems.forEach((item) => item.classList.toggle("menu-item"));
 	menubutton.classList.toggle("close");
 });
+
+// initialize display elements
+
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
