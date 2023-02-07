c5036a50ec31503ea673cdc44ad27b7e104f9acf
RosanaGarciap <rdlagp@byui.edu>
2023-02-07 18:53:43 -0400

    Updating JSON FILE

--- scripts/get-menu-links.js
+++ scripts/get-menu-links.js
@@ -1,4 +1,4 @@
-const url = "./scripts/assignments.json";
+const url = "https://rosanagarciap.github.io/wdd230/scripts/assignments.json";
 const list = document.querySelector('#menu');
 
 
@@ -7,21 +7,23 @@ const displayWeeks = (data) => {
     for (var week in data) {
         j = j+1;
       // Create elements to add to the list
-      let item = document.createElement('li');
-      let number = document.createElement('b');
-      number.textContent = `Week ${j}`;
+      const item = document.createElement('li');
+      const number = document.createElement('b');
+      number.textContent = `Week ${j}: `;
       item.appendChild(number)
       let i = 0;
-      week.forEach(element => {
-        let link = document.createElement('a');
-        link.textContent = `Assignment ${i} |`;
-        link.href = `${element}`; 
+      data[week].forEach(element => {
+        i=i+1;
+        const link = document.createElement('a');
+        link.textContent = `${element[1]} |`;
+        link.href = `${element[0]}`; 
         item.appendChild(link);       
       });
       
       list.appendChild(item);
 
     }; // end of arrow function and forEach loop
+    
   }
   
 
@@ -33,3 +35,36 @@ async function getWeekLinks() {
   }
 
 getWeekLinks();
+
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const urlClima = 'https://api.openweathermap.org/data/2.5/weather?q=Caracas,ve&appid=ea355e01fe2811da8431678b0cf96cf7&units=imperial';
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src',iconsrc);
+    weatherIcon.setAttribute('alt', `Icon ${desc}`);
+    captionDesc.textContent = `${desc}`;
+  }
+
+async function apiFetch() {
+    try {
+      const response = await fetch(urlClima);
+      if (response.ok) {
+        const data = await response.json();
+        //console.log(data); // testing only
+        displayResults(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  apiFetch();
