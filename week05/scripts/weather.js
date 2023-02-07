cb7ebda2f0a6f345eec07b68fa12043c835359ab
RosanaGarciap <rdlagp@byui.edu>
2023-02-07 05:30:39 -0400

    Weather API Learning Activity

---
+++ week05/scripts/weather.js
@@ -0,0 +1,33 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Trier,de&appid=ea355e01fe2811da8431678b0cf96cf7&units=imperial';
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src',iconsrc);
+    weatherIcon.setAttribute('alt', `Icon ${desc}`);
+    captionDesc.textContent = `${desc}`;
+  }
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        //console.log(data); // testing only
+        displayResults(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  apiFetch();
+
