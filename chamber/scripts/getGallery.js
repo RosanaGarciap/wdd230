6317bd13c1fbe297e2aaf019ad9f3481f04c8131
RosanaGarciap <rdlagp@byui.edu>
2023-02-14 17:07:34 -0400

    updating localstorage in discover page

--- chamber/scripts/getGallery.js
+++ chamber/scripts/getGallery.js
@@ -34,8 +34,8 @@ const displayGallery = (Gallerys) => {
       portrait.setAttribute('src', Gallery.url);
       portrait.alt = `Picture of ${GalleryFullName}`;// fill in the blank
       portrait.setAttribute('loading', 'lazy');
-      portrait.setAttribute('width', '200');
-      portrait.setAttribute('height', '200');
+      portrait.setAttribute('width', '150');
+      portrait.setAttribute('height', '150');
   
       // Append the section(card) with the created elements
       card.appendChild(portrait);
@@ -54,3 +54,40 @@ async function getGalleryData() {
   }
   
 getGalleryData();
+
+
+
+// initialize display elements
+
+const visitsDisplay = document.querySelector("#visits");
+const daysLeftOutput = document.querySelector("#daysleft");
+let today = new Date();
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls")); // Using the Number() function ensures that if the storage item does not exist, it will be converted into a zero (0) which helps on the if block condition.
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+    // Days from last visit
+
+    let lastVisit = new Date(localStorage.getItem("lastvisit"));
+    // check if is the waing days of December, if so ... change to next year.
+    let daysleft = (Date.now()-lastVisit.getTime()) / 84600000;
+
+    daysLeftOutput.innerHTML = `${daysleft.toFixed(
+        0
+    )} days<br> From last visit ${lastVisit}`;
+
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+    window.localStorage.setItem("lastvisit", today); 
+    daysLeftOutput.innerHTML = `0 days<br> From last visit`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+
+// show todays date demonstration
+
