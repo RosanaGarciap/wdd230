0938fd777b81f85e754d0803f2efd933d6b7fc61
RosanaGarciap <rdlagp@byui.edu>
2023-02-14 13:11:17 -0400

    add pictures

--- chamber/scripts/loading.js
+++ chamber/scripts/loading.js
@@ -71,10 +71,48 @@ const displayMembers = (members) => {
 async function getImageData() {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);
+    //console.table(data.Gallerys);
     displayMembers(data.members);
   }
   
 getImageData();
 
+
+const cards = document.querySelector('#gallery');
+
+const displayGallery = (Gallerys) => {
+    Gallerys.forEach((Gallery) => {
+      // Create elements to add to the div.cards element
+      let card = document.createElement('section');
+      let fullName = document.createElement('h2'); // fill in the blank
+
+      let portrait = document.createElement('img');
+  
+      // Build the h2 content out to show the Gallery's full name
+      let GalleryFullName = `${Gallery.title}`;
+      fullName.textContent = `${GalleryFullName} `; // fill in the blank
+      // Build the image portrait by setting all the relevant attributes
+      portrait.setAttribute('src', Gallery.url);
+      portrait.alt = `Picture of ${GalleryFullName}`;// fill in the blank
+      portrait.setAttribute('loading', 'lazy');
+      portrait.setAttribute('width', '200');
+      portrait.setAttribute('height', '200');
+  
+      // Append the section(card) with the created elements
+      card.appendChild(fullName); //fill in the blank
+      card.appendChild(portrait);
+  
+      cards.appendChild(card);
+    }); // end of arrow function and forEach loop
+  }
+  
+
+async function getGalleryData() {
+    const response = await fetch(url);
+    let datas = await response.json();
+    //console.table(data.Gallerys);
+    displayGallery(datas.gallery);
+  }
+  
+getGalleryData();
 
