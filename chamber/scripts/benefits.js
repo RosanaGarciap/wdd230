8c8717b1f617d4170fe031dc5ecba7136f2fd084
RosanaGarciap <rdlagp@byui.edu>
2023-02-17 23:20:18 -0400

    add benefits

--- chamber/scripts/benefits.js
+++ chamber/scripts/benefits.js
@@ -2,17 +2,16 @@
 const url = "https://rosanagarciap.github.io/wdd230/chamber/scripts/membership.json";
 const list = document.querySelector('#bn');
 const type = document.querySelector('#type');
+const level = document.createElement('h3'); // fill in the blank
 
-const displayBenefits = (Benefitss) => {
+const displayBenefits = (Benefitss) => { 
+  list.textContent = "";
     Benefitss.forEach((Benefits) => {
       // Create elements to add to the div.presentation element
       let card = document.createElement('li');
 
-      let b= document.createElement('p'); // fill benefit
-  
+      card.textContent = Benefits; 
       // Append the section(card) with the created elements
-      card.appendChild(b)
-
       list.appendChild(card);
     }); // end of arrow function and forEach loop
   }
@@ -21,13 +20,11 @@ const displayBenefits = (Benefitss) => {
 async function getBenefitsData() {
     const response = await fetch(url);
     let datas = await response.json();
-    const beneficio = document.getElementById("account").value;
-    let level = document.createElement('h3'); // fill in the blank
-    
+    const beneficio = document.getElementById("account").value;    
     if (beneficio == "np") {
         level.textContent = `Non-Profit`;
         type.appendChild(level);
-        displayBenefits(datas.nonprofit);
+        displayBenefits(datas.np);
     }
     if (beneficio == "bronze") {
         level.textContent = `Bronze`;
@@ -46,5 +43,3 @@ async function getBenefitsData() {
     }
     //console.table(data.Benefitss);
   }
-  
-getBenefitsData();
